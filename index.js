ec051928cfae73573aa8d2450ebd222b090bcd94
Yihong Wang <yh.wang@ibm.com>
2017-08-03 18:01:24 -0700

    Add cloudant service
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

--- index.js
+++ index.js
@@ -1,26 +1,70 @@
-var express = require("express");
-var app = express();
-var cfenv = require("cfenv");
-var bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const cfenv = require("cfenv");
+const bodyParser = require('body-parser');
+const Cloudant = require('cloudant');
+const config = require('./config.json');
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// parse application/json
+const services = cfenv.getAppEnv().getServices();
+const dbCred = services[config.dbService] && services[config.dbService]['credentials'];
+var dbconn;
+
 app.use(bodyParser.json());
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'IP table' });
+  if (!!dbconn) {
+    renderIPTable(req, res);
+  }
 });
 
 app.post('/add', function (req, res) {
-  console.log(req.body);
+  if (!!dbconn && !!req.body.hwaddr) {
+    var now = new Date();
+    dbconn.insert(
+      {
+        MAC: req.body.hwaddr,
+        IP: req.body.ipaddr,
+        Date: now.toUTCString(),
+        DateNum: now.getTime()
+      });
+  }
   res.json({msg: 'OK'});
 });
 
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("To view your app, open this link in your browser: http://localhost:" + port);
+  if (dbCred) {
+    Cloudant(
+      { url:dbCred.url,
+        username:dbCred.username,
+        password:dbCred.password
+      }, function(err, cloudant, reply) {
+
+      if (err) {
+        console.error(`unable to connect to cloudantNoSQLDB ${err.stack}`)
+      }
+
+      console.log('Connected to cloudantNoSQLDB');
+      dbconn = cloudant.db.use('ipboard');
+    });
+  }
 });
+
+function renderIPTable(req, res) {
+  dbconn.find({
+    "selector": { "DateNum": { "$gt": 0 } },
+    "fields": [ "MAC", "IP", "Date" ],
+    "sort": [ { "DateNum": "desc" } ],
+    "limit": 10
+  }, (err, result) => {
+    if (err) {
+      console.error(`can't query db: ${err.stack}`);
+    } else {
+      res.render('index', { title: 'IP table', docs: result.docs });
+    }
+  });
+}
